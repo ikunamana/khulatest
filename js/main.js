b7d25214bc462d77c9740e3730e9332a6eae1bee
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-03-17 23:44:52 +0400

    javascript change for registration div index

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ var signclick = document.getElementById("signinclick")
 var formsig = document.getElementById("formsig")
     regclick.onclick = function (regist) {
         formreg.style.transform = "scale(1)";
+        formreg.style.zIndex = "1";
         formsig.style.transform = "scale(0.8)";
         formsig.style.pointerEvents = "none";
         formsig.style.filter = "blur(30px)";
@@ -57,6 +58,7 @@ var formsig = document.getElementById("formsig")
     signclick.onclick = function(sign){
         // formsig.classList.toggle('classtogglesign');
         formreg.style.transform = "scale(0)";
+        formreg.style.zIndex = "-1";
         formsig.style.transform = "scale(1)";
         formsig.style.filter = "blur(0px)";
         formsig.style.pointerEvents = "auto";
