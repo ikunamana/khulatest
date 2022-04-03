22d4268cf8082be971b6b4c334749153936a7ebb
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-03 22:18:09 +0400

    new components

---
+++ js/products.js
@@ -0,0 +1,80 @@
+
+async function populate() {
+
+    const requestURL = 'https://raw.githubusercontent.com/ikunamana/khulatest/main/js/products.json';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+
+    populateProduktebi(superHeroes);
+
+}
+
+function populateProduktebi(obj) {
+    const section = document.getElementById('section');
+    const produkti = obj['production'];
+
+
+    for (const komponenti of produkti) {
+        const myDiv = document.createElement('div');
+        const firstDiv = document.createElement('div');
+        const imgDiv = document.createElement('div');
+        const imgOfFirstDiv = document.createElement('img');
+        const secondDiv = document.createElement('div');
+        const dasaxelebaII = document.createElement('p');
+        const nameOfOProductII = document.createElement('p');
+        const fasiCharchoII = document.createElement('p');
+        const actualPriceII = document.createElement('h2');
+        const btnshekvetaII = document.createElement('div');
+        const btnshekveta = document.createElement('button');
+        const kalataDiv = document.createElement('div');
+        const kalata = document.createElement('p');
+        const kalataimg = document.createElement('img');
+
+
+        myDiv.setAttribute("class", "allproduct");
+        firstDiv.setAttribute("class", "productform");
+        imgDiv.setAttribute("class", "productimg");
+        imgOfFirstDiv.setAttribute("class", "productimg1");
+        imgOfFirstDiv.setAttribute("src", `${komponenti.img}`);
+        secondDiv.setAttribute("class", "producttext");
+        btnshekvetaII.setAttribute("class", "butshekv");
+        btnshekveta.setAttribute("class", "btnshekvetapro");
+        kalataDiv.setAttribute("class", "cartdiv");
+        kalataimg.setAttribute ("class", "cartimg");
+        kalataimg.setAttribute("src","../images/cart1.png");
+
+        // kalata.textContent("კალათა");
+        // dasaxelebaII.innerHTML("<strong>დასახელება:</strong>");
+        // btnshekveta.innerHTML("შეკვეთა");
+        // fasiCharchoII.innerHTML("<strong>ფასი (ც):</strong>");
+
+        nameOfOProductII.textContent = `${komponenti.name}`;
+        actualPriceII.textContent = `${komponenti.price}`;
+
+        myDiv.append(firstDiv);
+        firstDiv.append(imgDiv);
+        imgDiv.append(imgOfFirstDiv);
+        firstDiv.append(secondDiv);
+        secondDiv.append(dasaxelebaII);
+        secondDiv.append(nameOfOProductII);
+        secondDiv.append(fasiCharchoII);
+        secondDiv.append(actualPriceII);
+        secondDiv.append(btnshekvetaII);
+        btnshekvetaII.append(btnshekveta);
+        btnshekvetaII.append(kalataDiv);
+        kalataDiv.append(kalataimg);
+        
+        section.append(myDiv);
+    }
+}
+
+
+
+populate();
+
+
+
+
+
