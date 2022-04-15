139c706e653d37d1c8e03bab72c2827a6544406d
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-15 21:12:47 +0400

    ka

--- js/cart.js
+++ js/cart.js
@@ -26,7 +26,7 @@ function removeCartItem(event) {
 
 function addToCartClicked(event) {
     var gilaki = event.target
-    var product = gilaki.parentElement.parentElement
+    var product = gilaki.parentElement.parentElement.parentElement
     var title = product.getElementsByClassName('cart-item-title')[0].innerText
     var price = product.getElementsByClassName('cart-item-price')[0].innerText
     var imageSrc = product.getElementsByClassName('productimg1')[0].src
