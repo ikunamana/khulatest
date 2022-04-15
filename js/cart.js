2c1e01966854aef663c9b60a9f47b07561ffde48
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-15 16:53:21 +0400

    wq

--- js/cart.js
+++ js/cart.js
@@ -3,5 +3,5 @@ var dropupmenu1 = document.getElementById("cart-content");
 
 dropbtn.onclick = function dropupmenu(){
     dropupmenu1.classList.toggle("cart-content");
-    dropbtn.classList.toggle("cart-contnet-active");
+    dropbtn.classList.toggle("cart-button-active");
 }
