c0d93a3bd06f4014b4fc0eab431954e463a3483a
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-15 22:30:24 +0400

    ikuna

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,8 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
-}
+// if (document.readyState == 'loading') {
+//     document.addEventListener('DOMContentLoaded', ready)
+// } else {
+//     ready()
+// }
 
 function ready() {
     var remCartItemButtons = document.getElementsByClassName('remove-button')
