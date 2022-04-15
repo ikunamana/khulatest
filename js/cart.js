ecf8a6045753d854a2d0fb75f9a354cb9f306245
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-15 18:33:22 +0400

    jh

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,23 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+function ready(){
+    var remCartItemButtons = document.getElementsByClassName('remove-button')
+    for (var i=0; i < remCartItemButtons.length; i++){
+        var button = remCartItemButtons[i]
+            button.addEventListener('click', removeCartItem)
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target 
+        buttonClicked.parentElement.parentElement.remove()
+}
+
+
+
 var dropbtn = document.getElementById("cartbtn");
 var dropupmenu1 = document.getElementById("cart-content");
 
