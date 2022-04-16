850f790eb9112f8cc512637a47ccb9014573db7a
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-16 12:19:57 +0400

    ikuna

--- js/products.js
+++ js/products.js
@@ -209,7 +209,7 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = total + 'GEL'
 }
-        }
+
 var dropbtn = document.getElementById("cartbtn");
 var dropupmenu1 = document.getElementById("cart-content");
 
@@ -219,7 +219,7 @@ dropbtn.onclick = function cartmenu() {
 }
 cartIsEmpty()
     updateCartTotal()
-}
+}}
 
 populate();
 
