a497602044e53bd2a1e02d26d7cfffdc78e38ffa
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-16 22:33:39 +0400

    jhjh

--- js/cart.js
+++ js/cart.js
@@ -34,7 +34,8 @@ function buttonDisabled() {
     var emptyCart = document.getElementById('emptycart')
     var orderButton = document.getElementById('orderButton')
 
-    if (emptyCart.style.display = "flex") {
+    if (emptyCart.style.display === "flex") {
+
         orderButton.classList.add('order-clear');
     } else {
         orderButton.classList.remove('order-clear');
