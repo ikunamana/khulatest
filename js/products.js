8b5d3d4c16049e75ffbcb1ba8ccb3053bcced941
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-20 00:35:39 +0400

    ikunamana

--- js/products.js
+++ js/products.js
@@ -258,7 +258,8 @@ function addItemToCart(title, price, imageSrc) {
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     var cartItemNames = document.getElementsByClassName('cart-item-name')
-    var priceCart = document.getElementsByClassName('item-quantity').value;
+    var productChildren = cartItems.childElement.childElement;
+    var priceCart = productChildren.getElementsByClassName('item-quantity').value;
     for (var i = 0; i < cartItemNames.length; i++) {
         if (cartItemNames[i].innerText == title) {
             ++priceCart;
