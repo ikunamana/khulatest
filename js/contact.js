0371d2a7f2537875f011ac923d27eedc5b19ae4a
ikunamana <98601663+ikunamana@users.noreply.github.com>
2022-04-23 22:31:14 +0400

    a

--- js/contact.js
+++ js/contact.js
@@ -6,15 +6,16 @@ arrow.addEventListener('click', contact)
 
 
     function contact(){
-        if(contactdiv.style.marginLeft = "-300px"){
+        while(contactdiv.style.marginLeft = "-300px"){
             contactdiv.style.marginLeft = "0px";
             arrowdiv.style.marginLeft = "200px";
             arrow.style.transform = "rotate(180deg)"
-        } else{
-                contactdiv.style.marginLeft = "-300px";
-                arrowdiv.style.marginLeft = "0px";
-                arrow.style.transform = "rotate(0)"
-            }
+         }
+         //else{
+        //         contactdiv.style.marginLeft = "-300px";
+        //         arrowdiv.style.marginLeft = "0px";
+        //         arrow.style.transform = "rotate(0)"
+        //     }
         }
 
 //------- END -------//
