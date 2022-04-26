45aaab3c372d7731d7ae717fd97284f11c5b1d96
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-26 16:58:13 +0400

    k

--- js/products.js
+++ js/products.js
@@ -110,7 +110,7 @@ function ready() {
     var quantityInputs = document.getElementsByClassName('item-quantity')
     for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
+        input.addEventListener('change', quantityChanged,updateCartTotal)
     }
     document.getElementsByClassName('order-button')[0].addEventListener('click', orderClicked)
     document.getElementsByClassName('order-clear')[0].addEventListener('click', orderClear)
