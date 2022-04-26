ba0589e5f8eee7586ac291913011d1ef055cc6e8
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-26 19:15:18 +0400

    jj

--- js/products.js
+++ js/products.js
@@ -234,7 +234,7 @@ function orderClear() {
 }
 
 function quantityChanged(event) {
-    var input = event.target;
+    var inputActive = event.target;
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
