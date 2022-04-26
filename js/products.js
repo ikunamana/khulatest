97df10f2582e5ad2853c49a04e21bac6b596c9fe
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-26 17:46:07 +0400

    k

--- js/products.js
+++ js/products.js
@@ -239,7 +239,7 @@ function orderClear() {
 
 function quantityChanged(event) {
     var input = event.target;
-    if (isNaN(input.value) || input.value <= 0 || input.value) {
+    if (isNaN(input.value) || input.value <= 0 || input.valuec) {
         input.value = 1
     }
     updateCartTotal()
