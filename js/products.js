24475f5177d1dd536cb695aca7cc6e4f303171f4
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-26 21:14:06 +0400

    k

--- js/products.js
+++ js/products.js
@@ -89,7 +89,11 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-  
+    var quantityInputs = document.getElementsByClassName('item-quantity')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('keyup', quantityChanged)
+    }
 
     var remCartItemButtons = document.getElementsByClassName('remove-button');
     for (var i = 0; i < remCartItemButtons.length; i++) {
@@ -230,17 +234,11 @@ function orderClear() {
 }
 
 function quantityChanged(event) {
-    var quantityInputs = document.getElementsByClassName('item-quantity')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i];
-        input.addEventListener('keyup', quantityChanged)
-    }
     var inputClicked = event.target;
     if (isNaN(inputClicked.value) || inputClicked.value <= 0) {
         inputClicked.value = 1
     }
     updateCartTotal()
-    quantityChanged()
 }
 
 function removeCartItem(event) {
