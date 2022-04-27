59f087197ef76d869a580410637dc88a31212c6c
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-27 19:11:15 +0400

    aa

--- js/products.js
+++ js/products.js
@@ -211,13 +211,13 @@ function orderClicked() {
                     var cartRowF = cartRow[i];
 
 
-                var orderedItems = cartRowF.getElementsByClassName('cart-item-name')
+                var orderedItems = cartRowF.getElementsByClassName('cart-item-name').innerHTML
                 var orderedQuantity = cartRowF.getElementsByClassName('item-quantity')[0]
                 var ikuna = document.createElement('div');
                 ikuna.innerHTML = "დასახელება:" + orderedItems + "რაოდენობა:" + orderedQuantity
-
-                console.log(orderedItems)
                 }
+                console.log(orderedItems)
+                
     // კალათაში არსებული პროდუქტების ერთიანად წაშლა შეკვეთის ღილაკზე კლიკის შემდეგ.
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
