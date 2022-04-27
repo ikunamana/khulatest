541f6e2b57bba16fc27f5fc0935925563667520d
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-27 20:08:04 +0400

    jjj

--- js/products.js
+++ js/products.js
@@ -221,11 +221,12 @@ function orderClicked() {
                         "type": orderedType,
                         "price": orderedPrice,
                 }]
+                orderObj = obj['order']
                 
                     
                 
                 
-                finalOrder.innerHTML = order
+                finalOrder.innerHTML = orderObj
                 console.log(finalOrder)
 
                 }
