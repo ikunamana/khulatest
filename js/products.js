5bf0af26f33ec475b09c6916b107c2e46a8b1585
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-28 20:51:34 +0400

    jjj

--- js/products.js
+++ js/products.js
@@ -220,7 +220,7 @@ function orderClicked() {
                         // raodeonoba: orderedQuantity,
                         // type: orderedType,
                         // price: orderedPrice
-                    },
+                    },{order}
                 
                         
                 var orderObj = [order][i]
