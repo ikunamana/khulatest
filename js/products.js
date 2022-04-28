55b10d79b635757934379c7ac43c5f88d9c6fec5
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-28 19:00:20 +0400

    kk

--- js/products.js
+++ js/products.js
@@ -229,7 +229,7 @@ function orderClicked() {
                 
                 // finalOrder.innerHTML = orderObj
                 console.log(ordProd)
-                localStorage.setItem('ordered', JSON.stringify(ordProd))
+                localStorage.setItem('ordered'+[i], JSON.stringify(ordProd))
                 
         
     }
