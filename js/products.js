bf51166753a23da4476f56baa1447bc2e2370ad3
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-28 20:14:06 +0400

    das

--- js/products.js
+++ js/products.js
@@ -215,11 +215,12 @@ function orderClicked() {
                 var orderedType = cartRowF.getElementsByClassName('cart-item-type')[0].innerHTML
                 var orderedPrice = cartRowF.getElementsByClassName('cart-item-price')[0].innerHTML
                 var finalOrder = document.createElement('div');
-                var order ={
+                var order ={ "ordered": [{
                         name: orderedItems,
                         raodeonoba: orderedQuantity,
                         type: orderedType,
-                        price: orderedPrice}
+                        price: orderedPrice}]
+                }
                         
                 var orderObj = [][i]
 
