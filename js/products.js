caeb155f66ef4208900d118e3876dbcdf5880797
Ikuna Birbichadze <98601663+ikunamana@users.noreply.github.com>
2022-04-28 19:35:29 +0400

    s

--- js/products.js
+++ js/products.js
@@ -221,10 +221,10 @@ function orderClicked() {
                         type: orderedType,
                         price: orderedPrice}
                         
-                
+                var ordProd = []
 
-                var orderObj = JSON.stringify(order[i])
-                var ordProd = [orderObj[i]]
+                orderObj = JSON.stringify(order[i])
+                ordProd = [orderObj[i]]
                 // ordProd.push(orderObj[i])
             
                 
